d1f6621f88506e61a8b83d49d525a8242cd12b7d
Yonghua Zheng <yh.zheng@yuneec.com>
2022-02-07 13:12:21 +0800

    Refine GMS with BingMap NativeSDK

--- Android/app/src/main/java/org/opendroneid/android/app/AircraftMapView.java
+++ Android/app/src/main/java/org/opendroneid/android/app/AircraftMapView.java
@@ -7,9 +7,16 @@
 package org.opendroneid.android.app;
 
 import androidx.annotation.NonNull;
+import androidx.core.content.res.ResourcesCompat;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.graphics.Color;
+import android.graphics.PointF;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import androidx.annotation.Nullable;
 import android.util.Log;
@@ -19,28 +26,47 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.Manifest;
 import androidx.core.app.ActivityCompat;
+import androidx.viewpager2.widget.ViewPager2;
+
 import android.content.pm.PackageManager;
-import com.google.android.gms.maps.CameraUpdateFactory;
-import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.model.CameraPosition;
-import com.google.android.gms.maps.OnMapReadyCallback;
-import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.BitmapDescriptorFactory;
-import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.Marker;
-import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
+import android.widget.LinearLayout;
+
+import com.microsoft.maps.Geopath;
+import com.microsoft.maps.Geopoint;
+import com.microsoft.maps.Geoposition;
+import com.microsoft.maps.MapAnimationKind;
+import com.microsoft.maps.MapCamera;
+import com.microsoft.maps.MapElement;
+import com.microsoft.maps.MapElementLayer;
+import com.microsoft.maps.MapElementTappedEventArgs;
+import com.microsoft.maps.MapIcon;
+import com.microsoft.maps.MapImage;
+import com.microsoft.maps.MapPolyline;
+import com.microsoft.maps.MapProjection;
+import com.microsoft.maps.MapRenderMode;
+import com.microsoft.maps.MapScene;
+import com.microsoft.maps.MapStyleSheet;
+import com.microsoft.maps.MapStyleSheets;
+import com.microsoft.maps.MapTappedEventArgs;
+import com.microsoft.maps.MapView;
+import com.microsoft.maps.OnMapElementTappedListener;
+
+import org.opendroneid.android.BuildConfig;
+import org.opendroneid.android.R;
 import org.opendroneid.android.data.AircraftObject;
 import org.opendroneid.android.data.LocationData;
 import org.opendroneid.android.data.SystemData;
 import org.opendroneid.android.data.Util;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.List;
 
-public class AircraftMapView extends SupportMapFragment implements OnMapReadyCallback, GoogleMap.OnMarkerClickListener {
+public class AircraftMapView extends Fragment {
     private static final String TAG = "AircraftMapView";
-    private GoogleMap googleMap;
+    private MapView mapView;
+    private MapElementLayer pinLayer;
+    private MapImage markerImage;
+    private MapImage markerPilotImage;
     private AircraftViewModel model;
 
     private final HashMap<AircraftObject, MapObserver> aircraftObservers = new HashMap<>();
@@ -86,7 +112,7 @@ public class AircraftMapView extends SupportMapFragment implements OnMapReadyCal
 
             @Override
             public void onChanged(@Nullable AircraftObject object) {
-                if (object == null || object.getLocation() == null || googleMap == null)
+                if (object == null || object.getLocation() == null)
                     return;
                 MapObserver observer = aircraftObservers.get(object);
                 if (observer == null)
@@ -95,46 +121,36 @@ public class AircraftMapView extends SupportMapFragment implements OnMapReadyCal
                 if (object.getLocation().getLatitude() == 0.0 && object.getLocation().getLongitude() == 0.0)
                     return;
 
-                LatLng ll = new LatLng(object.getLocation().getLatitude(), object.getLocation().getLongitude());
+                Geopoint ll = new Geopoint(object.getLocation().getLatitude(), object.getLocation().getLongitude());
                 Log.i(TAG, "centering on " + object + " at " + ll);
 
                 if (last != null && last.marker != null) {
-                    last.marker.setAlpha(0.5f);
+                    last.marker.setOpacity(0.5f);
                     if (last.markerPilot != null)
-                        last.markerPilot.setAlpha(0.5f);
+                        last.markerPilot.setOpacity(0.5f);
                 }
                 if (observer.marker != null)
-                    observer.marker.setAlpha(1.0f);
+                    observer.marker.setOpacity(1.0f);
                 if (observer.markerPilot != null)
-                    observer.markerPilot.setAlpha(1.0f);
+                    observer.markerPilot.setOpacity(1.0f);
 
                 last = observer;
 
-                CameraPosition position = googleMap.getCameraPosition();
-                if (position.zoom < DESIRED_ZOOM - ALLOWED_ZOOM_MARGIN || position.zoom > DESIRED_ZOOM + ALLOWED_ZOOM_MARGIN)
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ll, DESIRED_ZOOM));
-                else
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLng(ll));
+                double zoom = mapView.getZoomLevel();
+                if (zoom < DESIRED_ZOOM - ALLOWED_ZOOM_MARGIN || zoom > DESIRED_ZOOM + ALLOWED_ZOOM_MARGIN)
+                    zoom = DESIRED_ZOOM;
+                mapView.setScene(
+                        MapScene.createFromLocationAndZoomLevel(ll, zoom),
+                        MapAnimationKind.NONE);
             }
         });
     }
 
-    @Override
-    public boolean onMarkerClick(@NonNull Marker marker) {
-
-        Object tag = marker.getTag();
-        if (tag instanceof AircraftObject) {
-            model.setActiveAircraft((AircraftObject) tag);
-            return true;
-        }
-        return false;
-    }
-
     class MapObserver implements Observer<LocationData> {
-        private Marker marker;
-        private Marker markerPilot;
-        private Polyline polyline;
-        private PolylineOptions polylineOptions;
+        private MapIcon marker;
+        private MapIcon markerPilot;
+        //private Polyline polyline;
+        //private PolylineOptions polylineOptions;
 
         private final AircraftObject aircraft;
 
@@ -142,56 +158,58 @@ public class AircraftMapView extends SupportMapFragment implements OnMapReadyCal
             aircraft = active;
             aircraft.location.observe(AircraftMapView.this, this);
             aircraft.system.observe(AircraftMapView.this, systemObserver);
-            polylineOptions = new PolylineOptions()
-                    .color(Color.RED)
-                    .clickable(true);
+            //polylineOptions = new PolylineOptions()
+            //        .color(Color.RED)
+            //        .clickable(true);
         }
 
         void stop() {
             aircraft.location.removeObserver(this);
             aircraft.system.removeObserver(systemObserver);
             if (marker != null) {
-                marker.remove();
+                pinLayer.getElements().remove(marker);
                 marker = null;
             }
             if (markerPilot != null) {
-                markerPilot.remove();
+                pinLayer.getElements().remove(markerPilot);
                 markerPilot = null;
             }
-            if (polyline != null) {
-                polyline.remove();
-                polyline = null;
-            }
-            polylineOptions = null;
+            //if (polyline != null) {
+            //    polyline.remove();
+            //   polyline = null;
+            //}
+            //polylineOptions = null;
         }
 
         private final Observer<SystemData> systemObserver = new Observer<SystemData>() {
             @Override
             public void onChanged(@Nullable SystemData ignore) {
                 SystemData sys = aircraft.getSystem();
-                if (sys == null || googleMap == null)
+                if (sys == null)
                     return;
 
                 // filter out zero data
                 if (sys.getOperatorLatitude() == 0.0 && sys.getOperatorLongitude() == 0.0)
                     return;
 
-                LatLng latLng = new LatLng(sys.getOperatorLatitude(), sys.getOperatorLongitude());
+                Geopoint latLng = new Geopoint(sys.getOperatorLatitude(), sys.getOperatorLongitude());
                 if (markerPilot == null) {
                     String id = "ID missing";
                     if (aircraft.getIdentification1() != null)
                         id = aircraft.getIdentification1().getUasIdAsString();
-                    markerPilot = googleMap.addMarker(
-                            new MarkerOptions()
-                                    .alpha(0.5f)
-                                    .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                                    .position(latLng)
-                                    .title(sys.getOperatorLocationType().toString() + ": " + id));
-                    if (markerPilot != null)
-                        markerPilot.setTag(new Pair<>(aircraft, this));
+                    markerPilot = new MapIcon();
+                    markerPilot.setTitle(sys.getOperatorLocationType().toString() + ": " + id);
+                    markerPilot.setOpacity(0.5f);
+                    if (markerPilotImage != null) {
+                        markerPilot.setImage(markerPilotImage);
+                        markerPilot.setNormalizedAnchorPoint(new PointF(0.5f, 1f));
+                    }
+                    markerPilot.setLocation(latLng);
+                    markerPilot.setTag(new Pair<>(aircraft, this));
+                    pinLayer.getElements().add(markerPilot);
+                } else {
+                    markerPilot.setLocation(latLng);
                 }
-                if (markerPilot != null)
-                    markerPilot.setPosition(latLng);
             }
         };
 
@@ -199,47 +217,68 @@ public class AircraftMapView extends SupportMapFragment implements OnMapReadyCal
         public void onChanged(@Nullable LocationData ignore) {
             boolean zoom = false;
             LocationData loc = aircraft.getLocation();
-            if (loc == null || googleMap == null || polylineOptions == null)
+            if (loc == null)
                 return;
 
             // filter out zero data
             if (loc.getLatitude() == 0.0 && loc.getLongitude() == 0.0)
                 return;
 
-            LatLng latLng = new LatLng(loc.getLatitude(), loc.getLongitude());
+            Geopoint latLng = new Geopoint(loc.getLatitude(), loc.getLongitude());
             if (marker == null) {
                 String id = "ID missing";
                 if (aircraft.getIdentification1() != null)
                     id = aircraft.getIdentification1().getUasIdAsString();
-                marker = googleMap.addMarker(
-                        new MarkerOptions()
-                                .alpha(0.5f)
-                                .position(latLng)
-                                .title("aircraft " + id));
-                if (marker != null)
-                    marker.setTag(aircraft);
+                marker = new MapIcon();
+                marker.setTitle("aircraft " + id);
+                marker.setOpacity(0.5f);
+                marker.setImage(markerImage);
+                marker.setNormalizedAnchorPoint(new PointF(0.5f, 1f));
+                marker.setLocation(latLng);
+                marker.setTag(aircraft);
+                pinLayer.getElements().add(marker);
+
                 zoom = true;
-            }
+            } else
+                marker.setLocation(latLng);
 
-            polylineOptions.add(latLng);
-            if (polyline != null) {
-                polyline.remove();
-                polyline = null;
-            }
-            polyline = googleMap.addPolyline(polylineOptions);
+            //polylineOptions.add(latLng);
+            //if (polyline != null) {
+            //    polyline.remove();
+            //    polyline = null;
+            //}
+            //polyline = googleMap.addPolyline(polylineOptions);
 
-            marker.setPosition(latLng);
             if (zoom) {
-                googleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+                mapView.setScene(MapScene.createFromLocation(latLng), MapAnimationKind.NONE);
             }
         }
     }
 
     @Override @NonNull
     public View onCreateView(@NonNull LayoutInflater layoutInflater, ViewGroup viewGroup, Bundle bundle) {
-        View view = super.onCreateView(layoutInflater, viewGroup, bundle);
-        getMapAsync(this);
-        return view;
+        final LinearLayout parent = (LinearLayout) layoutInflater.inflate(R.layout.fragment_aircraftmap, viewGroup, false);
+        mapView = parent.findViewById(R.id.map);
+        mapView.onCreate(bundle);
+        mapView.setCredentialsKey(BuildConfig.CREDENTIALS_KEY);
+        pinLayer = new MapElementLayer();
+        pinLayer.addOnMapElementTappedListener(new OnMapElementTappedListener() {
+            @Override
+            public boolean onMapElementTapped(MapElementTappedEventArgs mapElementTappedEventArgs) {
+                List<MapElement> elementList = mapElementTappedEventArgs.mapElements;
+                for (MapElement element : elementList) {
+                    Object tag = element.getTag();
+                    if (tag instanceof AircraftObject) {
+                        model.setActiveAircraft((AircraftObject) tag);
+                        return true;
+                    }
+                }
+                return false;
+            }
+        });
+        mapView.getLayers().add(pinLayer);
+        markerImage = getPinImage();
+        return parent;
     }
 
     @Override
@@ -249,16 +288,57 @@ public class AircraftMapView extends SupportMapFragment implements OnMapReadyCal
     }
 
     @Override
-    public void onMapReady(@NonNull GoogleMap googleMap) {
-        if (getActivity() == null)
-            return;
-
-        this.googleMap = googleMap;
-        setMapSettings();
+    public void onStart() {
+        super.onStart();
+        mapView.onStart();
     }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    //@Override
+    //public void onMapReady(@NonNull GoogleMap googleMap) {
+    //    if (getActivity() == null)
+    //        return;
+    //
+    //    setMapSettings();
+    //}
+
     public void setMapSettings() {
-        if (getActivity() == null || googleMap == null)
+        if (getActivity() == null)
             return;
 
         if (ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED &&
@@ -266,10 +346,24 @@ public class AircraftMapView extends SupportMapFragment implements OnMapReadyCal
             return;
         }
 
-        googleMap.getUiSettings().setMyLocationButtonEnabled(true);
-        googleMap.getUiSettings().setMapToolbarEnabled(false);
-        googleMap.setMyLocationEnabled(true);
-        googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
-        googleMap.setOnMarkerClickListener(this);
+        //googleMap.getUiSettings().setMyLocationButtonEnabled(true);
+        //googleMap.getUiSettings().setMapToolbarEnabled(false);
+        //googleMap.setMyLocationEnabled(true);
+        //googleMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        //googleMap.setOnMarkerClickListener(this);
+    }
+
+    private MapImage getPinImage() {
+        Drawable drawable = ResourcesCompat.getDrawable(getResources(), R.drawable.ic_pin, null);
+
+        int width = drawable.getIntrinsicWidth();
+        int height = drawable.getIntrinsicHeight();
+        Bitmap bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888);
+
+        Canvas canvas = new Canvas(bitmap);
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return new MapImage(bitmap);
     }
 }
